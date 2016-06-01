2f0b74bfea674fb73210a097a61feef2e49df155
babaykaVadym <babayka1991@gmail.com>
2016-06-01 21:57:01 +0300

    First commint

--- vadym_donets/src/main/java/action03/App.java
+++ vadym_donets/src/main/java/action03/App.java
@@ -6,8 +6,7 @@ package action03;
  */
 public class App 
 {
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello Worldsdsdsd!" );
+    public static void main( String[] args ) {
+        System.out.println( "Hello Worldsdsdxxxsd!" );
     }
 }
