aab11939fcf8fc32e82e29f8f28503afe8103f62
Lisa <Lisa@Lisa-PC>
2016-06-01 22:00:51 +0300

    my first project in github

---
+++ Evgeniya_Opanasenko/src/main/java/Evgeniya_Opanasenko/App.java
@@ -0,0 +1,13 @@
+package Evgeniya_Opanasenko;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World! Second PART" );
+    }
+}
