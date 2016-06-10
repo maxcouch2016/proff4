18839edea3cbb5f1f8d0789b05ce5fa4cfd611ff
ufoma <ufoma@DESKTOP-MILJBTD>
2016-06-10 17:53:25 +0300

    calc

--- pavel_putrenkov/src/main/java/action04/MiniCalc.java
+++ pavel_putrenkov/src/main/java/action04/MiniCalc.java
@@ -2,12 +2,21 @@ package action04;
 
 
 public class MiniCalc {
-	public int multiply(int a, int b){
-		int res = (a/2) * 2 * b;
-		return res;
-	}
 	public int multiplyInt(int a, int b){
-		int res = (a/2) * 2 * b;
+		int res = (a * b);
 		return new Integer(res);
 	}
+	public Double multiplyDouble(int a, int b){
+		double res = (a * b);
+		return new Double(res);
+	}
+	 public Double divide(Integer a, Integer b){
+		 double res = (a / b);
+		 return new Double (res);  
+	 }
+		public int divideyInt(int a, int b){
+			int res = (a* b);
+			return res;
 }
+}		
+
